0d7415af1a7b32c0d3a1083d9ad9a2fa5b062c66
Fyodor <fiodorlukashov@gmail.com>
2025-01-08 21:51:14 +0200

    Abstract blacklist class

---
+++ src/managers/blacklist.py
@@ -0,0 +1,33 @@
+from src.managers import FileManager
+
+
+class BlackList:
+    """Class to add accounts to blacklist."""
+
+    @staticmethod
+    def get_blacklist() -> dict:
+        """
+        return
+            {account_phone: [groups]}
+        """
+        return FileManager.read_blacklist()
+
+    @staticmethod
+    def add_to_blacklist(
+        account_phone: str,
+        group: str
+    ) -> None:
+        FileManager.add_to_blacklist(
+            account_phone,
+            group
+        )
+
+    @staticmethod
+    def is_group_blacklisted(
+        account_phone: str,
+        group: str
+    ) -> bool:
+        blacklist = FileManager.read_blacklist()
+        return group in blacklist.get(
+            account_phone, []
+        )
